6e74e77330f81fa9e10cb093541c0e77c168af84
yusayalcin <yusayalcin6@gmail.com>
2020-06-15 12:57:02 +0300

    şifremi unuttum tamamlandı

--- app/src/main/java/com/ahmetaliyilmaz/aay19/bidenco/signin_activity.java
+++ app/src/main/java/com/ahmetaliyilmaz/aay19/bidenco/signin_activity.java
@@ -6,8 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -17,48 +17,59 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class signin_activity extends AppCompatActivity {
-//call firebase
+    //call firebase
     private FirebaseAuth firebaseAuth;
-//call text and password from activity signin xml
+    //call text and password from activity signin xml
     EditText emailText, passwordText;
-//create activity
+    TextView forgot_password;
+
+    //create activity
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-//define firebase,e-mail and text
+        //define firebase,e-mail and text
         firebaseAuth = FirebaseAuth.getInstance();
         emailText = findViewById(R.id.emailText);
         passwordText = findViewById(R.id.passwordText);
-//call back user information
+        forgot_password = findViewById(R.id.forgot_password);
+
+        forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(signin_activity.this, ResetPasswordActivity.class));
+            }
+        });
+        //call back user information
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-        if (firebaseUser != null){
-            Intent intent = new Intent(signin_activity.this,feed_activity.class);
+        if (firebaseUser != null) {
+            Intent intent = new Intent(signin_activity.this, feed_activity.class);
             startActivity(intent);
             finish();
         }
     }
-//define sign in click
-    public void signInClicked (View view){
-//export to string email and password information
+
+    //define sign in click
+    public void signInClicked(View view) {
+        //export to string email and password information
         String email = emailText.getText().toString();
         String password = passwordText.getText().toString();
-//define firebase in signin clicked and check to success
-        firebaseAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+        //define firebase in signin clicked and check to success
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
             @Override
             public void onSuccess(AuthResult authResult) {
-//if it is success, open feed activity
-                Intent intent = new Intent (signin_activity.this, feed_activity.class);
+                //if it is success, open feed activity
+                Intent intent = new Intent(signin_activity.this, feed_activity.class);
                 startActivity(intent);
                 finish();
-// if it is fail, open pop-up message
+                // if it is fail, open pop-up message
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
 
-                Toast.makeText(signin_activity.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+                Toast.makeText(signin_activity.this, e.getLocalizedMessage().toString(), Toast.LENGTH_LONG).show();
 
             }
         });
@@ -66,13 +77,8 @@ public class signin_activity extends AppCompatActivity {
     }
 
     //with post button open signup activity
-    public void postButton (View view){
-
-         Intent intent = new Intent(signin_activity.this,signup_activity.class);
-         startActivity(intent);
-
+    public void postButton(View view) {
+        Intent intent = new Intent(signin_activity.this, signup_activity.class);
+        startActivity(intent);
     }
-
-
 }
-
