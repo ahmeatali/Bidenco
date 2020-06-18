1ad49a3644c701774f33b161309ecab1a48cf2ca
yusayalcin <yusayalcin6@gmail.com>
2020-06-18 13:52:14 +0300

    iletişim güncelleme tamamlandı

--- app/src/main/java/com/ahmetaliyilmaz/aay19/bidenco/SupportActivity.java
+++ app/src/main/java/com/ahmetaliyilmaz/aay19/bidenco/SupportActivity.java
@@ -1,17 +1,14 @@
 package com.ahmetaliyilmaz.aay19.bidenco;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SupportActivity extends AppCompatActivity {
     Button send;
-    EditText message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,25 +16,21 @@ public class SupportActivity extends AppCompatActivity {
         setContentView(R.layout.activity_support);
 
         send = findViewById(R.id.send);
-
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                message = findViewById(R.id.message);
-                String m = message.getText().toString();
-                sendEmail(m);
+                sendEmail();
             }
         });
     }
 
-    public void sendEmail(String message) {
+    public void sendEmail() {
         Intent intent = new Intent(Intent.ACTION_SEND);
         String[] to = new String[]{"yusayalcin6@gmail.com"};
         String subject = "Bidenco/Destek";
 
         intent.putExtra(Intent.EXTRA_EMAIL, to);
         intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-        intent.putExtra(Intent.EXTRA_TEXT, message);
 
         intent.setType("message/rfc822");
         startActivity(Intent.createChooser(intent, "Send Email"));
